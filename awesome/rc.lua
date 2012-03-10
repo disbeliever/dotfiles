6cbb1547d2aba203733c5e6049abc1b381fb4c3c
disbeliever <nerevar101@mail.ru>
2012-03-10 11:45:43 +0400

    little fix for xxkb doesn't shows tray icon correctly after restart

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -466,4 +466,6 @@ end)
 
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
+awesome.add_signal("exit", function(c) awful.util.spawn("killall -9 xxkb") end)
 -- }}}
