1c79827b5e9602b03293b422a95500f42b1dcf63
disbeliever <nerevar101@mail.ru>
2015-08-15 08:21:07 +0400

    firefox/user.js: more paranoid settings

--- firefox/user.js
+++ firefox/user.js
@@ -1,3 +1,5 @@
 user_pref("browser.selfsupport.url", "");
 user_pref("browser.safebrowsing.enabled", false);
+user_pref("browser.safebrowsing.downloads.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
 user_pref("browser.safebrowsing.malware.enabled", false);
